b615ff2b414d7bf4d355e4339187b2efc9c5ab5a
hiraekram10 <heraiqram123@gmail.com>
2024-05-04 20:10:30 +0500

    familiar operator done

--- Javascript/1.strings/index.js
+++ Javascript/1.strings/index.js
@@ -2,4 +2,112 @@ var studentName  = "anoosha";
 // alert('student')
 
 
-alert(studentName)
+// alert(studentName)
+
+var userName = "Maliha" ;
+
+// alert("userName")
+// console.log(userName)
+// console.log(hi)
+
+var email;   // declaration
+// console.log(email);
+email = "herai@gmail.com"  //initaialize
+// console.log(email);
+
+
+
+
+var num = 6
+var num2  = 4
+
+       
+ var sum = num + num2 
+ var sub = num - num2
+ var mul= num  * num2
+ 
+ var division = num / num2
+ var moduluss = num % num2
+
+// console.log(num + num2);
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+// console.log(division);
+// console.log(moduluss);
+
+// update 
+var price = 200
+// console.log(price);
+
+// price = 300  reassign
+// console.log(price);
+
+
+price = price + 5
+// console.log(price); 
+
+// illegal
+// - var first name
+// - var &name   
+// var var  alert 
+// var 1user
+// var 123
+// 
+
+
+// legal
+// - var userAlert
+// var myVar
+// var $price  _price
+// var user1
+
+/*camelCase
+
+myName
+userEmail
+lastName
+fruitsName 
+*/
+
+
+/*snack_case
+foo_bar 
+hello_world */
+
+// js case sensitive
+
+// var flower = "rose"
+// var Flower = "black rose"
+
+// console.log(Flower);
+
+
+var num = "5"
+console.log(num + 2);    //concatination
+console.log(num + "2");
+console.log(num - 1);
+console.log(num - 2 + (2-1));
+console.log("23"* 5);
+
+console.log(3 - "53");
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
