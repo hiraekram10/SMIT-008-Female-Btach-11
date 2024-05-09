62ef6a51926903c95907fad6044d416328f19b1f
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-09 20:13:12 +0500

    done with postfix and prefix

---
+++ Javascript/2unfamiliar/index.js
@@ -0,0 +1,66 @@
+
+
+// var num = 5
+
+// console.log(num);  //5
+// num = num + 3
+// console.log(num);  //8
+
+// num = num + 1  // num++  post increent
+//                // ++num preincrement 
+
+
+// console.log("num++",num++); //5
+// console.log("++num:",++num);  //6
+
+// var numOne = 2  //3
+// var numTwo = numOne++  //numone  num 
+// console.log("num 1:", numOne); //3
+// console.log("num 2:", numTwo); //2
+
+
+var a = 2;  //3 //4
+var b = 5; //6  //7
+
+console.log(a);
+console.log(b);
+         //   2   + 5    + 3  + 6   
+var result = a++  + b++ + a++ + b++   //19 16  14
+console.log(result);
+
+console.log(a);
+console.log(b);
+
+// pre inc
+        //   3   +   6   + 4  +  7
+// var result = ++a  + ++b + ++a + ++b   //18   20  14 
+// console.log(result);
+// post inc  
+
+
+var num1 = 3  //2  //3 //4
+var num2 = 5  //6  //5
+        //     2   +   5   +   2    +   5   +      3
+var result = --num1 +  num2++ + num1++  + --num2 + num1++
+console.log(result);
+
+
+console.log("2" - "2"); 
+
+
+var a = "2" + "2"
+
+var b = "2" - "2"
+
+console.log(typeof a) 
+console.log(typeof b);
+
+var c = +prompt("Enter your number","5")  //2
+console.log(c +2);
+var j;
+
+console.log(j);
+
+
+
+
