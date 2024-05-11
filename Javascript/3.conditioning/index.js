cca98a539cb76d94b222f93134866a161e40aaea
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-11 20:21:39 +0500

    done

---
+++ Javascript/3.conditioning/index.js
@@ -0,0 +1,65 @@
+
+
+// var isMarried = false
+// 2 == 2  true   comparison operator
+ // 2 === "2"  value, dataType
+//  5 != "5"  true
+// 5 !== "5" 
+// 10 < 9  false
+// 9> 3 true
+// 10 <= 11  true
+
+
+// 3>=1  true greater than or equal to
+
+
+ /*
+var num = 2
+ 
+if(num === "2"){
+  console.log("bat sach hogai");
+}else{
+    console.log("jhoot bat hai");
+}
+
+*/
+//6 falsy values
+// "",false,0,undefined,null,Nan
+
+ 
+ 
+// if("3" !== 3){
+//     console.log("bat sach hogai");
+//   }else{
+//       console.log("jhoot bat hai");
+//   }
+
+// var  passengerAge = +prompt()
+var adult = 5
+var old = 50
+
+// if(passengerAge <= 4){
+//     console.log("according to your age " + passengerAge + "we will not charge" );
+// }else if(adult <= passengerAge){
+//    console.log("you are an adult now " + passengerAge + " you have to pay 50 rs");
+// }else if (old <= passengerAge){
+//     console.log("sorry sir but you have to pay 25rs");
+// }
+
+// else{
+//     console.log("you have to pay");
+// }
+
+var userInp =+ prompt()
+
+var modulus = userInp % 2 
+
+
+if(modulus === 0){
+    console.log("number is even");
+}else{
+    console.log("odd number");
+}
+
+
+
