c1a35547eed647ef34a43dc5daca9ef31a3a12f4
= <83424343+wasifa-khan@users.noreply.github.com>
2024-05-23 19:28:33 +0500

    arrays started

---
+++ Javascript/5.Nested/index.js
@@ -0,0 +1,71 @@
+
+/*
+var 11 = 3// numbers not allowed
+var 1city = "karachi "
+var $names = 
+var @name
+var "name" ="hira"
+
+*/
+
+
+
+// var name;   //declare
+// console.log(name);  //undefined
+
+
+// console.log(numbers) //number is not defined
+
+
+// var check =4
+
+// console.log(check > 3 && check === 4);
+// console.log(check < 6 || check == 4);
+
+
+// !true => false
+// !false => true
+
+
+// var isMarried  = true
+
+
+
+//  if(!isMarried){
+//     console.log("if chla");
+//  }else{
+//     console.log("else chla");
+//  }
+
+
+/*
+nested condition
+
+var education = "Bs"
+var exper = 6
+
+
+if (education == "Bs" && exper > 1) {
+    if (exper < 5) {
+  console.log('tmajao kl say bad main laat mardengay hm ')
+    } else {
+console.log("tm ameer ho bhaeee kahin or dhondo");
+    }
+} else {
+    console.log('you are not')
+}*/
+
+// var student0 = "maha"
+// var students1 = "rida"
+
+
+var stuents =  [ "maha","rida","haya","emaan","ayesha", 5]
+console.log(stuents);
+
+
+
+
+
+
+
+
