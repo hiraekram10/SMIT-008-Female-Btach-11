ef826368c3a81be5620f505de56f9761666a92db
= <heraiqram123@gmail.com>
2024-05-30 20:11:12 +0500

    DONE WITH METHODS

--- Javascript/7.arraysMethod/index.js
+++ Javascript/7.arraysMethod/index.js
@@ -1,1 +1,24 @@
-var names  = ["hira"]
+var names = ["rida","aina","areeba"]
+// var check = names.splice(1,2,"saba")
+// console.log(check);
+// console.log(names[1])
+
+
+var copyNames = names
+console.log(names);
+console.log(copyNames);
+
+
+copyNames[1] = "saba"
+console.log(names);
+console.log(copyNames);
+
+
+var num = 1
+var copynum = num
+console.log(num);
+console.log(copynum);
+
+copynum = 3
+console.log(num);
+console.log(copynum);
