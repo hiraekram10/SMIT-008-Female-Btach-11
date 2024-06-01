b45a4224658890980049e09647bd6c57dc1cfb24
= <heraiqram123@gmail.com>
2024-06-01 20:08:32 +0500

    satrted loops

---
+++ Javascript/8.loops/index.js
@@ -0,0 +1,82 @@
+// var name = "hira";
+// var copyName = name
+// console.log(name);
+// console.log(copyName);
+
+
+// copyName = "rida"
+// console.log(name);
+// console.log(copyName);
+
+
+// var nums = [5,4,6,3]
+// var copyNums = nums
+// console.log(nums);
+// console.log(copyNums);
+
+
+// nums[2] = 8
+// console.log(nums);
+// console.log(copyNums);  // shallow copy
+
+// var veges = ["carrot","potato","lowkey","teenday","bhindi",];
+// var copyVeges = veges.slice(2,5)
+// console.log(veges);
+// console.log(copyVeges);
+// copyVeges[1]= "onion"
+
+// console.log(veges);
+// console.log(copyVeges);(3)//  ['lowkey', 'onion', 'bhindi']
+
+
+// var copyVeges = veges.slice(-1)
+// console.log(copyVeges);
+
+
+
+
+// var veges = ["carrot","lowkey","teenday","bhindi","lemon"];
+// veges.pop()
+// veges.shift()
+// veges.shift()
+// veges.push("bangan")
+// veges.splice(0,1,"harimirch")
+// console.log(veges)
+
+
+
+
+
+
+// console.log("good morning");
+
+// console.log("good morning");
+
+// console.log("good morning");
+
+// console.log("good morning");
+
+
+// Syntax
+// for (initialization;condition;updation){
+// }
+
+
+for(var i = 0;i <= 4 ;i++){
+   console.log("good morning");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
