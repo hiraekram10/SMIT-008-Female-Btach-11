17e86899ea2fec8e1a8d3737299b55c01f91f7eb
= <umrabadar555@gmail.com>
2024-06-06 20:05:07 +0500

    done with flag

---
+++ Javascript/9.arrays_loop/index.js
@@ -0,0 +1,76 @@
+// for(var i = 0; i <= 5; i++){
+//      console.log(i)
+    
+// }
+
+// console.log(i);
+
+
+// for(var i = 5; i < 10; i++){
+//     console.log(i)
+   
+// }
+
+// for(var i = 1; i <= 10; i++){
+//    document.write(2 + " X " + i + " = " + 2*i + "<br>")
+   
+// }
+
+// for(var i = 10; i >= 1; i--){
+//     document.write(2 + " X " + i + " = " + 2*i + "<br>")
+    
+//  }
+
+
+// var names = ["ayesha","haya","rabia"]
+
+
+// for(var i = names.length-1; i >= 0 ; i--){
+//     console.log(names[i])
+// }
+
+
+// var countries = ["Pakistan","India","China","Iran"]
+// var flag = false
+
+// for(var i = 0; i < countries.length ; i++){
+//     // console.log(countries[i])
+//     if(countries[i] == "India"){
+       
+//         flag = true
+       
+//     }
+    
+// }
+
+// if(flag){
+//  console.log("mil gya");
+// }else{
+//     console.log("nhi mila");
+// }
+
+// var userInp = prompt("enter your country")
+
+// var countries = ["Pakistan","India","China","Iran"]
+// var flag = "no"
+
+// for(var i = 0; i < countries.length ; i++){
+//     // console.log(countries[i])
+//     if(countries[i] == userInp){
+       
+//         flag = "yes"
+       
+//     }
+    
+// }
+
+// if(flag == "yes"){
+//  console.log("mil gya");
+// }else{
+//     console.log("nhi mila");
+// }
+
+
+var nums = [1,2,3,5,8,[9,6]]
+console.log(nums[5]);
+
