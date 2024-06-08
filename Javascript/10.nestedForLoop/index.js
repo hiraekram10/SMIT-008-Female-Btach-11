fade02176768f4116c5dbd48b284e5bdcf03c6ee
= <umrabadar555@gmail.com>
2024-06-08 20:05:16 +0500

    nested for loop

---
+++ Javascript/10.nestedForLoop/index.js
@@ -0,0 +1,46 @@
+    
+
+    // for(var i = 0;i<5; i++){
+    //     for(var j =1; j < 3; j++){
+    //         console.log(i,j);
+    //     }
+    // }
+
+
+    // var arr = [["apple",'mango'],["bnana","orange"],["grapes","guava"]]
+
+
+    // for(var i= 0;i <arr.length ; i++){
+    //     // console.log(arr[i]);
+    //     for(var j = 0; j < arr[i].length ;j++){
+    //         console.log(arr[i][j]);
+    //     }
+    // }
+
+
+
+//   for(var i = 0; i < 5;i++){
+//     if(i == 2){
+//         console.log(i); //2
+//         break;
+//     }
+//     console.log(i);  //0,1
+   
+//   }
+//   console.log(i);  //2
+
+
+for(var i = 0; i < 5;i++){
+    if(i === 3){
+        break;
+        console.log(i);
+    }
+    console.log(i); 
+      
+  }
+  console.log(i++);
+  console.log (--i);
+ 
+  
+
+
