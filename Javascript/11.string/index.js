a4e29265e88dd8e81c70dd40deb80153932b18db
= <83424343+wasifa-khan@users.noreply.github.com>
2024-06-13 20:10:54 +0500

    strings strated

---
+++ Javascript/11.string/index.js
@@ -0,0 +1,36 @@
+//quiz q   quesries
+
+var fruitName = "water melon"
+var result = fruitName.slice(2,7)
+console.log(result);
+
+
+
+var arr = [2,3,4]
+arr.length = 0
+arr.push(6)
+console.log(arr);
+
+
+var name = "hira"
+name[0] = "b"  
+console.log(name); //hira  
+console.log(name.length);  //4
+console.log(name[3]);  //a
+
+
+var veg = "poTato"
+console.log(veg.toLowerCase());
+
+// var userInp  = prompt()
+// console.log(userInp.toLowerCase() === "karachi");
+
+
+
+var myVar = prompt("enter your word")
+var charFirst = myVar.slice(0,1)
+var remaining =myVar.slice(1)
+console.log(charFirst.toUpperCase() + remaining);
+
+
+
