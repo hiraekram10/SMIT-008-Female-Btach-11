6617d38948ac0c3607174c64f390a2423e366d2c
= <umrabadar555@gmail.com>
2024-06-15 20:00:46 +0500

    done with string methods

---
+++ Javascript/12.stringMethod/index.js
@@ -0,0 +1,31 @@
+// var fruitName = "water"
+// // fruitName = fruitName + "melon"
+// fruitName += "melon"
+// console.log(fruitName);
+
+
+// var word = prompt("Enter word")
+// var opposite = ""
+
+// for(var i = word.length-1; i >= 0  ; i--){
+//     opposite += word[i]
+// }
+
+// if(word === opposite){
+//     alert("Its a palindrome ")
+// }else{
+//     alert("Its not a palindrome ")
+// }
+
+
+var sentense = "hi how are you"
+console.log(sentense.indexOf("o"));
+console.log(sentense.lastIndexOf('o'))
+
+
+var arr = ["apple","bnana","orange"]
+console.log(arr.indexOf("bnana"));
+
+
+
+
