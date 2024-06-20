4e7b0cd09b2f1fd42b01011d6f92567bfaa87e10
= <umrabadar555@gmail.com>
2024-06-20 20:04:54 +0500

    Done with string methods

---
+++ Javascript/13.strloop/index.js
@@ -0,0 +1,105 @@
+// var fName = "hira ekram  ali "
+// var result = ''
+// for (var i = 0; i < fName.length; i++) {
+//     if (i == 0) {
+//         result += fName[i].toUpperCase()
+//     }else if(fName[i-1]  === " "){
+//         result += fName[i].toUpperCase()
+
+
+//     }
+//      else {
+//         result += fName[i]
+//     }
+// }
+// console.log(result);
+
+
+// var sentence = "Hi my name is hira"
+// for(var i = 0; i < sentence.length;i++){
+//     if(sentence.slice(i,i + 4) === "hira"){
+//         console.log('found');
+//     };
+// }
+
+
+// var sentence = "Hi my name is hira"
+// var userWord = prompt("Enter your word",'hira')
+// for(var i = 0; i < sentence.length;i++){
+//     if(sentence.slice(i,i + userWord.length) === userWord){
+//         console.log('found');
+//     };
+// }
+
+// var sentence = "Hi my name is hira"
+// // console.log(sentence.indexOf("bb"));
+// var user = prompt('word')
+
+// if(sentence.indexOf(user) != -1){
+//     console.log('found');
+// }else{
+//     console.log('not found');
+// }
+
+//  lastIndexOf() not found  // -1
+
+
+// var words = 'my school is best school'
+// // console.log(words);
+// // console.log(words.replace('school',"university"));
+// // console.log(words.replaceAll('school',"university"));
+// console.log(words.charAt(4));
+
+
+/*
+var words = ["pen","book","flower","tree","meat"]
+
+var secWord = words[1];
+var flag = false
+
+var playerInp = prompt("guess")
+
+for(var i = 0; i< words.length;i++){
+    if(playerInp === secWord){
+       flag = true
+       break
+    }
+}
+
+if(flag){
+    console.log('win');
+}else{
+    console.log('out');
+} */
+
+
+//ROUND
+let a = Math.round(75.60);
+let b = Math.round(2.50);
+let c = Math.round(2.49);
+let d = Math.round(-2.60);
+let e = Math.round(-2.50);
+let f = Math.round(-2.49);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
