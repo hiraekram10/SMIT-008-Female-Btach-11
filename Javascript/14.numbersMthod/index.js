8aa1eac2687deac4eb5c622344e321e46a5bbe03
= <umrabadar555@gmail.com>
2024-06-22 20:03:18 +0500

    done with math

---
+++ Javascript/14.numbersMthod/index.js
@@ -0,0 +1,57 @@
+// var num = Math.ceil(2.567);
+// console.log(num); //3
+
+
+
+// var num = Math.ceil(2.367);
+// console.log(num); //3
+
+
+// var num = Math.ceil(2.056);
+// console.log(num); //3
+
+
+// var num = Math.ceil(2.0);
+// console.log(num); //2
+
+
+
+// var num = Math.floor(67.23)
+// console.log(num);   //67 
+
+
+// var num = Math.floor(67.0)
+// console.log(num); 
+
+
+// var num = Math.random() * 6
+// console.log(Math.floor(num));   //0-5 
+
+
+
+// var num =parseFloat(prompt("enter number"))
+// console.log(num +3);
+
+//parseInt("98.3467")  // 98  round off
+Number("878.67")  // //878.67
+
+
+
+var floatingNumString = "24.9876";
+ var num = Number(floatingNumString);
+ console.log(typeof num);
+
+
+
+ var numberAsNumber = 1234;
+var numberAsString = numberAsNumber.toString();
+console.log(numberAsString + 5);
+
+
+
+var num1 = 89.92347278346874
+console.log(num1.toFixed(4));
+ 
+
+
+ 
