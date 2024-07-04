ce64ebf999e30a1ffb5339d5be74b3918154a7fd
= <83424343+wasifa-khan@users.noreply.github.com>
2024-07-04 20:13:00 +0500

    functions done

---
+++ Javascript/17.functionend/index.js
@@ -0,0 +1,76 @@
+// function yourFunc (one,two){
+//     var fruitName = one + " " + two
+//     console.log(fruitName);
+// }
+
+// yourFunc("melon")  // mwlon undefined
+
+
+
+// function sub (one,two ){
+//     var subtract = one +two
+//     console.log(subtract);
+// }
+
+
+// var userInp = +prompt("enter your number")
+// var userInp2 = +prompt("enter your second number ")
+// sub(userInp,userInp2)
+
+
+
+// var user = prompt()
+// console.log(user);
+// ======================== Return =============
+
+//The return statement stops the execution 
+// of a function and returns a value.
+
+// function myFunction (a ,b){
+//    return a + b 
+   
+  
+// }
+
+// // console.log(myFunction(3,4))   
+// var reult = myFunction(3,5) 
+// console.log(reult);
+
+
+// function myFunction (a ,b){
+//     var sum =  a + b
+    
+//     return sum
+    
+   
+//  }
+ 
+ 
+// console.log(myFunction(4,5)+2)
+
+
+// function output (a,b){
+//     var sum = a + b
+//     return sum
+// } 
+// console.log(output(2,3));
+
+
+// function fruitChecking(fruit){
+//    var fruitList = ["apple","mango","bnana","orange"]
+//    if(fruitList.indexOf(fruit) !== -1){
+//     return "lai hon"
+//    }else{
+//     return " nhi lai hon"
+//    }
+// }
+
+// console.log(fruitChecking("grapes"));
+var sum = 3 + 2
+
+function add (){
+    var sum = 9
+    console.log(sum);
+}
+console.log(sum);
+
