48a5cd8badeba03f5ca41e265d61b1b294c49318
= <umrabadar555@gmail.com>
2024-07-20 20:02:53 +0500

    done with quiz

---
+++ Javascript/22.studentsqueries/index.js
@@ -0,0 +1,26 @@
+typeOf "Hello"
+
+var name = "Hello World"
+name.indexOf("o") //4
+
+
+var name = "Hello World"
+name.replace("o","x")  //'Hellx World'
+
+function percent (height,weight){
+  return height * weight
+}
+ var num = percent(4)
+ console.log(num);//aurgument matter
+
+
+
+ function cal (a , b){
+    return a +b / 2
+ }
+ console.log(cal(2,1));  //2.5 bodmas
+
+
+
+
+
