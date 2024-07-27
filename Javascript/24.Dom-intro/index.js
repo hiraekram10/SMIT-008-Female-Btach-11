12b1b69157de270b12d922b32a703215a9909754
= <umrabadar555@gmail.com>
2024-07-27 20:12:03 +0500

    done

---
+++ Javascript/24.Dom-intro/index.js
@@ -0,0 +1,16 @@
+var para = document.getElementsByTagName("p")
+// console.log(para);
+
+
+var paraTwo = document.getElementsByClassName('two')
+// console.log(paraTwo);
+
+var paraThree = document.getElementById("paraThree")
+// console.log(paraThree);
+
+var first = document.querySelector("p")
+var second = document.querySelectorAll(".two")
+var third = document.querySelector("#paraThree")
+console.log(first);
+console.log(second);
+console.log(third);
