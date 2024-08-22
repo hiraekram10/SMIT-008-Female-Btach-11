fe9a7599bff4b2783c5e7e4d602fa5b358539b83
= <umrabadar@555gmail.com>
2024-08-22 19:55:05 +0500

    done with objects

---
+++ Javascript/30.constructors/index.js
@@ -0,0 +1,109 @@
+
+// function Buscuitmold (){
+//     this.name = "prince";
+//     this.color = "brown";
+//     this.shape= "circle"
+
+
+// }
+
+
+// console.log(new Buscuitmold())
+
+// console.log(new Buscuitmold())
+
+// console.log(new Buscuitmold())
+
+// console.log(new Buscuitmold())
+
+// console.log(new Buscuitmold())
+
+// function Student (name,email,rollno){
+//     this.name = name;
+//     this.email = email;
+//     this.rollno= rollno
+
+
+// }
+
+
+//  var student1=  new Student("eman","eman@gmail.com","54e")
+//  console.log(student1);
+ 
+
+//  var student2=  new Student("asfar","asfar@gmail.com","64e")
+//  console.log(student2.email);
+ 
+
+//  var student3=  new Student("ali","ali@gmail.com","84e")
+//  console.log(student3);
+
+
+//  console.log(this);
+
+
+
+
+
+//  function Obj (name,number){
+//     this.name = name;
+//     this.number = number;
+//     this.gettingInfo = function (){
+//         console.log(this.name ,"name" ,this.number,"number");
+        
+//     }
+//  }
+
+
+//  var a = new Obj("rayan",897698)
+//  console.log(a.gettingInfo());
+
+
+
+ //_________________________ prototype
+ //A prototype is an existing inbuilt functionality in JavaScript
+//  Prototypes allow for inheritance so that objects can inherit properties 
+//  and methods from their parent object
+
+
+// function  Board (stName){
+//     this.stName = stName;
+    
+// }
+
+// Board.prototype.allDetails = function(){
+//     return this.stName
+// }
+
+ 
+// var board = new Board ("rida")
+// var board1 = new Board ("shifa")
+// console.log(board);
+// console.log(board1);
+
+// var obj = {
+//     name : "hira",
+//     email :"hira@gmail.com",
+//     }
+
+//     var keys = Object.keys(obj)
+
+
+// // console.log(Object.keys(obj));
+
+// for(var i = 0;i< keys.length;i++){
+//    console.log(obj[keys[i]])
+// }
+
+    
+
+
+
+
+
+
+
+
+ 
+ 
+ 
