da980299d43c5720163a25ba67ea0fb812d7b4ee
Umra-Badar20 <umrabadar555@gmail.com>
2025-02-20 18:16:06 +0500

    done with backend two

--- backend-development/one/index.js
+++ backend-development/one/index.js
@@ -1,19 +1,26 @@
 
-import express from 'express'
+import express, { response } from 'express' 
 
 const app =express()
 const port = 5000
 
+app.use(express.json())
 
+let users=[]
 app.get('/',(req,res)=>{
     res.send("Hello user")
 })
-app.get('/users',(req,res)=>{
-    res.send("Hello all users")
+app.get("/users",(req,res)=>{
+    res.send(users)
 })
-app.get('/users',(req,res)=>{
-    res.send("Hello all users")
+
+app.post('/user',(req,res)=>{
+       users.push(req.body)
+    res.send(users)
+      
 })
+
+
 
 app.listen(port,()=>{
      console.log(`server is running.. at ${port} `);
